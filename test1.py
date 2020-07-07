3033d4e450ca0a56f4c77e1c54d9de200cf441cc
lidenghui1-os <lidenghui1-os@360os.com>
2020-07-07 21:30:23 +0800

    定时发送

--- test1.py
+++ test1.py
@@ -1,9 +1,10 @@
+import time
 import requests
-import xlwt
 import json
-import os
 
-def req_info(a, d):
+
+
+def req_air(a, d):
     body = '{"stype":2,"dCty":"' + a + '","aCty":"' + d + '","flag":1,"start":"","end":"","classLevels":["Y"],' \
                                                           '"head":{"cid":"09031126310322327181","ctok":"",' \
                                                           '"cver":"1.0","lang":"01","sid":"8888","syscode":"09",' \
@@ -22,44 +23,25 @@ def req_info(a, d):
         "origin": "https://flights.ctrip.com"
 
     }
-    workbook = xlwt.Workbook(encoding='utf-8')
-    worksheet = workbook.add_sheet('prices')
-    title = ['日期', '出发', '到达', '价格']
-    row = 0
-    col = 0
 
-    for t in title:
-        worksheet.write(row, col, t)
-        col += 1
-    row = 1
     response = requests.post(url=url, data=body, headers=headers)
     prices_list = json.loads(response.text)
-    print(prices_list)
-
     for item in prices_list['prices']:
-        col = 0
-        if item['price'] is not None:
-            data = [item['dDate'], a, d, item['price']]
-            for one in data:
-                print('row:{},col:{}'.format(row, col))
-                worksheet.write(row, col, one)
-                col += 1
-            row += 1
-    file_name = './results/'+'{} to {}'.format(a, d) + '.xls'
-    if os.path.exists(file_name):
-        os.remove(file_name)
-    workbook.save(file_name)
+        t = item['dDate']
+        timearry = time.strptime(t, '%Y-%m-%d')
+        r_time = int(time.mktime(timearry))
+        if item['price'] is not None and r_time < 1596211200:
+            print(t)
 
 
 if __name__ == '__main__':
-    if not os.path.exists('./results'):
-        os.mkdir('results')
+
     city_dic = {'PAR': 'SHA', 'AMS': 'SHA', 'FRA': ['SHA', 'PVG', 'NKG']}
     keys_list = city_dic.keys()
     for a_city in keys_list:
         b_city = city_dic[a_city]
         if not isinstance(b_city, list):
-            req_info(a_city, b_city)
+            req_air(a_city, b_city)
         else:
             for d in b_city:
-                req_info(a_city, d)
+                req_air(a_city, d)
